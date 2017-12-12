1c4dc2bc01cf9d1a452aa8bc91e14591415114f5
User <User@DESKTOP-O22TPS7>
2017-12-12 15:37:15 -0500

    Added Classes and LinkedList Fields Luke

---
+++ src/linkedlistlak/LinkedList.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlistlak;
+
+/**
+ *
+ * @author User
+ */
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+}
